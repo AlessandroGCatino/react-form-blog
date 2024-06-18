35e73ce8c2f884cf036349f0af341fa84fd7b45e
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-06-18 16:35:24 +0200

    conclusione esercizio base

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { MdDeleteForever } from "react-icons/md";
 export default function(){
 
     const [list, setList] = useState([]);
@@ -6,6 +7,11 @@ export default function(){
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        setList([...list, title]);
+        setTitle('');
+    }
+    const deleteElement = (index) => {
+        setList(list.filter((el, i) => i!== index))
     }
 
     return(
@@ -14,11 +20,26 @@ export default function(){
             <div className="create">
                 <form onSubmit={handleSubmit}>
                     <label>Insert title: </label>
-                    <input type="text"/>
+                    <input type="text"
+                    value={title}
+                    onChange={ e => setTitle(e.target.value)}
+                    />
+                    <button type="submit"> Crea </button>
                 </form>
 
             </div>
             <h2>Created Articles</h2>
+            <div className="show">
+                <ol>
+                    {list.map((item, index) => 
+                    <li key={index}>
+                        {item}
+                        <MdDeleteForever 
+                        color="red"
+                        onClick={() => deleteElement(index)}/>
+                    </li>)}
+                </ol>
+            </div>
         </div>
     )
 }
