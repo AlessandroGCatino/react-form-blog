c67c17d816d713115f263faac740a8eb5aff7d30
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-06-18 17:23:07 +0200

    test per modifica del titolo

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
-import { MdDeleteForever } from "react-icons/md";
+import { MdDeleteForever, MdModeEdit } from "react-icons/md";
 export default function(){
 
-    const [list, setList] = useState([]);
+    const [list, setList] = useState(["Test", "Prova"]);
     const [title, setTitle] = useState('');
+    const [active, setActive] = useState(false);
+    const [edit, setEdit] = useState(null);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -14,31 +16,59 @@ export default function(){
         setList(list.filter((el, i) => i!== index))
     }
 
+    const handleEdit = (index, bool) => {
+        setActive(bool);
+        setEdit(index);
+    }
+
+
     return(
         <div>
             <h1>Create a new Article</h1>
-            <div className="create">
-                <form onSubmit={handleSubmit}>
-                    <label>Insert title: </label>
-                    <input type="text"
-                    value={title}
-                    onChange={ e => setTitle(e.target.value)}
-                    />
-                    <button type="submit"> Crea </button>
-                </form>
+            {
+                !active && 
+                    <div className="create">
+                        <form onSubmit={handleSubmit}>
+                            <label>Insert title: </label>
+                            <input type="text"
+                            value={title}
+                            onChange={ e => setTitle(e.target.value)}
+                            />
+                            <button type="submit"> Crea </button>
+                        </form>
 
-            </div>
+                    </div>
+            }
             <h2>Created Articles</h2>
-            <div className="show">
-                <ol>
-                    {list.map((item, index) => 
-                    <li key={index}>
-                        {item}
-                        <MdDeleteForever 
-                        color="red"
-                        onClick={() => deleteElement(index)}/>
-                    </li>)}
-                </ol>
+            <div className="index">
+                <div className="show">
+                    <ol>
+                        {list.map((item, index) => 
+                        <li key={index}>
+                            <span>{item}</span>
+                            <div>
+                                <MdModeEdit 
+                                color="blue"
+                                onClick={() => handleEdit(index, true)}/>
+                                <MdDeleteForever 
+                                color="red"
+                                onClick={() => deleteElement(index)}/>
+                            </div>
+                        </li>)}
+                    </ol>
+                </div>
+                {active && 
+                    <div className="edit">
+                        <form >
+                            
+                        </form>
+                        <label>New title:</label>
+                        <input type="text"
+                        placeholder={list[edit]}
+                        onChange={ e => setTitle(e.target.value)}
+                        />
+                    </div>
+                }
             </div>
         </div>
     )
